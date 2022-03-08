760a380df524ba6398a647320d6c94174fb6a52f
Leeyunji981020 <twister1114@gmail.com>
2022-03-08 15:11:57 +0900

    sample

---
+++ 입출력과사칙연산/sample.java
@@ -0,0 +1,11 @@
+package 입출력과사칙연산;
+
+import java.util.Scanner;
+import Util.*;
+public class sample{
+    static Scanner scanner = new Scanner(System.in);
+ public static void main(String[] args) {
+    System.out.println("샘플");
+    
+}
+}
